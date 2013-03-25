79db7eacab724b98e099391cd1d01e6c200a3785
Michel Bystranowski <michel@vertige.org>
2013-03-25 14:23:30 +0100

    personnalisation du template de page

--- sites/all/themes/easpd/templates/page.tpl.php
+++ sites/all/themes/easpd/templates/page.tpl.php
@@ -1,31 +1,29 @@
-<div class="row">
-  <?php if ($linked_site_name || $linked_logo): ?>
-    <div class="five columns">
-      <?php if ($linked_logo): ?>
-        <?php print $linked_logo; ?>
-      <?php endif; ?>
-    </div>
-    <div class="seven columns text-right">
-      <?php if ($is_front): ?>
-        <h1 id="site-name"><?php print $linked_site_name; ?></h1>
-      <?php else: ?>
-        <div id="site-name"><?php print $linked_site_name; ?></div>
-      <?php endif; ?>
-    </div>
-  <?php endif; ?>
-</div>
-
 <?php if (!empty($page['header'])): ?>
-  <div class="row">
-    <div class="twelve columns">
+  <div id="top">
+    <div id="language_switcher">
       <?php print render($page['header']);?>
     </div>
   </div>
 <?php endif; ?>
 
+<div id="header">
+  <?php if ($linked_site_name || $linked_logo): ?>
+    <div id="logo">
+      <?php if ($linked_logo): ?>
+        <?php print $linked_logo; ?>
+      <?php endif; ?>
+    </div>
+  <?php endif; ?>
+    <div id="slogan">
+      <?php
+        print t("EASPD is the <i>European Association of Service Providers for Persons with Disabilities</i> and represents 10.000 social service provider organisations across Europe and across disability.");
+      ?>
+    </div>
+</div>
+
 <?php if ($main_menu_links || !empty($page['navigation'])): ?>
-  <div class="row">
-    <nav class="twelve columns">
+  <div id="top_nav">
+    <nav>
       <?php if (!empty($page['navigation'])): ?>
         <?php print render($page['navigation']);?>
       <?php else: ?>
@@ -35,15 +33,8 @@
   </div>
 <?php endif; ?>
 
-<?php if ($site_slogan): ?>
-  <div class="row">
-    <div class="twelve columns panel radius">
-      <?php print $site_slogan; ?>
-    </div>
-  </div>
-<?php endif; ?>
-<div class="row">
-  <div id="main" class="<?php print $main_grid; ?> columns">
+<div id="page">
+  <div id="main">
     <?php if ($breadcrumb): print $breadcrumb; endif; ?>
     <?php if ($messages): print $messages; endif; ?>
     <?php if (!empty($page['help'])): print render($page['help']); endif; ?>
@@ -70,17 +61,19 @@
     <?php endif; ?>
     <?php print render($page['content']); ?>
   </div>
-  <?php if (!empty($page['sidebar_first'])): ?>
-    <div id="sidebar-first" class="<?php print $sidebar_first_grid; ?> columns sidebar">
-      <?php print render($page['sidebar_first']); ?>
-    </div>
-  <?php endif; ?>
-  <?php if (!empty($page['sidebar_second'])): ?>
-    <div id="sidebar-second" class="<?php print $sidebar_sec_grid; ?> columns sidebar">
-      <?php print render($page['sidebar_second']); ?>
-    </div>
-  <?php endif; ?>
-</div>
+  <div id="sidebar">
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <div id="sidebar-first" class="<?php print $sidebar_first_grid; ?> columns sidebar">
+        <?php print render($page['sidebar_first']); ?>
+      </div>
+    <?php endif; ?>
+    <?php if (!empty($page['sidebar_second'])): ?>
+      <div id="sidebar-second" class="<?php print $sidebar_sec_grid; ?> columns sidebar">
+        <?php print render($page['sidebar_second']); ?>
+      </div>
+    <?php endif; ?>
+  </div>
+</div><!-- end page -->
 <?php if (!empty($page['footer_first']) || !empty($page['footer_middle']) || !empty($page['footer_last'])): ?>
   <footer class="row">
     <?php if (!empty($page['footer_first'])): ?>
