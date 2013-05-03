e1b2286230fe8bfd6633301c4ca6a35967eb6e46
Michel Bystranowski <michel@vertige.org>
2013-05-03 10:36:27 +0200

    style du footer

--- sites/all/themes/easpd/templates/page.tpl.php
+++ sites/all/themes/easpd/templates/page.tpl.php
@@ -80,19 +80,19 @@
   </div>
 </div><!-- end page -->
 <?php if (!empty($page['footer_first']) || !empty($page['footer_middle']) || !empty($page['footer_last'])): ?>
-  <footer class="row">
+  <footer>
     <?php if (!empty($page['footer_first'])): ?>
-      <div id="footer-first" class="four columns">
+      <div id="footer-first">
         <?php print render($page['footer_first']); ?>
       </div>
     <?php endif; ?>
     <?php if (!empty($page['footer_middle'])): ?>
-      <div id="footer-middle" class="four columns">
+      <div id="footer-middle">
         <?php print render($page['footer_middle']); ?>
       </div>
     <?php endif; ?>
     <?php if (!empty($page['footer_last'])): ?>
-      <div id="footer-last" class="four columns">
+      <div id="footer-last">
         <?php print render($page['footer_last']); ?>
       </div>
     <?php endif; ?>
