cc2607a701b2b765da917a475badf088d7261625
Michel Bystranowski <michel@vertige.org>
2013-05-21 14:34:53 +0200

    configuration de ckeditor et révision de la typographie générale

---
+++ sites/all/themes/easpd/ckeditor.styles.js
@@ -0,0 +1,22 @@
+/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+/*
+ * This file is used/requested by the 'Styles' button.
+ * The 'Styles' button is not enabled by default in DrupalFull and DrupalFiltered toolbars.
+ */
+if(typeof(CKEDITOR) !== 'undefined') {
+    CKEDITOR.addStylesSet( 'drupal',
+    [
+        { name : 'Blue'      , element: 'span', attributes: { 'class': 'bleu' } },
+        { name : 'Pale Blue' , element: 'span', attributes: { 'class': 'bleu-pale' } },
+        { name : 'Light Blue', element: 'span', attributes: { 'class': 'bleu-clair' } },
+        { name : 'Green'     , element: 'span', attributes: { 'class': 'vert' } },
+        { name : 'Turquoise' , element: 'span', attributes: { 'class': 'turquoise' } },
+        { name : 'Purple'    , element: 'span', attributes: { 'class': 'violet' } },
+        { name : 'Orange'    , element: 'span', attributes: { 'class': 'orange' } },
+        
+    ]);
+}
