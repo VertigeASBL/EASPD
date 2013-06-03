3fcd0f693969e580c7cd6e1edef236450a54f1e0
Michel Bystranowski <michel@vertige.org>
2013-06-03 10:59:41 +0200

    ajoute les blocs "sociaux"…

--- sites/all/themes/easpd/templates/html.tpl.php
+++ sites/all/themes/easpd/templates/html.tpl.php
@@ -68,5 +68,17 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+
+  <?php if (drupal_is_front_page()) { ?>
+<!-- Giving away our users' privacy for an ugly like box -->
+<div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+  <?php } ?>
 </body>
 </html>
