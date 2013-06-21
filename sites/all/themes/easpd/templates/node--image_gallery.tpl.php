e4027c7d682f6f9903ad0e1a7d11903f06cc5fd9
Michel Bystranowski <michel@vertige.org>
2013-06-21 12:23:11 +0200

    ajoute les alt dans le template de gallerie d'image

--- sites/all/themes/easpd/templates/node--image_gallery.tpl.php
+++ sites/all/themes/easpd/templates/node--image_gallery.tpl.php
@@ -1,13 +1,20 @@
 <div id="node-<?php print $node->nid; ?>" class="node-gallery">
   <?php
   $imgcount = count($node->field_img['und']);
+
   for ($i = 0; $i < $imgcount; $i++) {
-    $image_uri = $node->field_img['und'][$i]['uri'];
-  // url
+
+    $image     = $node->field_img['und'][$i];
+    $image_uri = $image['uri'];
+    // url
     $masthead_raw = image_style_url('thumbnail', $image_uri);
     ?>
-    <a href="<?php print file_create_url($node->field_img['und'][$i]['uri']); ?>" rel="lightbox[<?php print $node->nid; ?>]">
-      <img class="image<?php print ($i + 1);?>" src="<?php print $masthead_raw; ?>" />
+    <a href="<?php print file_create_url($image_uri); ?>"
+        rel="lightbox[<?php print $node->nid; ?>]">
+
+      <img alt="$image['alt']" class="image<?php print ($i + 1);?>"
+            src="<?php print $masthead_raw; ?>" />
     </a>
+
   <?php } ?>
 </div>
