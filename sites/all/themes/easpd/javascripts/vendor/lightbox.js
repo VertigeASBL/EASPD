b7cf3f3867d027483cf2d09c6d9f304e1b44d874
Michel Bystranowski <michel@vertige.org>
2013-07-09 11:26:55 +0200

    charge correctement les images de la lightbox

--- sites/all/themes/easpd/javascripts/vendor/lightbox.js
+++ sites/all/themes/easpd/javascripts/vendor/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = 'images/loading.gif';
-      this.fileCloseImage = 'images/close.png';
+      this.fileLoadingImage = 'sites/all/themes/easpd/images/lightbox/loading.gif';
+      this.fileCloseImage = 'sites/all/themes/easpd/images/lightbox/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
