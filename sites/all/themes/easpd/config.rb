d1bd20ca7f8629d0213e37d9959822eee5c0909b
Michel Bystranowski <michel@vertige.org>
2014-03-20 10:46:11 +0100

    ajoute les css générés par sass dans le dépôt
    ça permet d'être plus sympa avec ceux qui n'utilisent pas sass

--- sites/all/themes/easpd/config.rb
+++ sites/all/themes/easpd/config.rb
@@ -13,7 +13,7 @@ javascripts_dir = "js"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
-output_style = (environment == :production) ? :compressed : :expanded
+output_style = :expanded
 
 # nécessaire pour FireSass, casse tout avec IE…
 sass_options = (environment == :production) ? {} : {:debug_info => true}
