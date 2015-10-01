f69ce00d78f9c5ed7df0917a51052c56d344493f
Michel Bystranowski <michel@vertige.org>
2015-10-01 16:43:21 +0200

    ajoute une classe pour le texte en blanc et passe le bleu pâle en blanc
    … pour plus de contraste sur le easy2read sans tout ré-encoder

--- sites/all/themes/easpd/ckeditor.styles.js
+++ sites/all/themes/easpd/ckeditor.styles.js
@@ -18,6 +18,7 @@ if(typeof(CKEDITOR) !== 'undefined') {
         { name : 'Turquoise' , element: 'span', attributes: { 'class': 'turquoise' } },
         { name : 'Purple'    , element: 'span', attributes: { 'class': 'violet' } },
         { name : 'Orange'    , element: 'span', attributes: { 'class': 'orange' } },
+        { name : 'White'     , element: 'span', attributes: { 'class': 'white' } },
 
         /* block styles */
         // { name : 'Red Box'         , element: 'div', attributes:{ 'class': 'boite-rouge' } },
